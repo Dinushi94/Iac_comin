d6bac8bec6a9d124bfae8376685e3cbb4ceb76a7
dinushi94 <dinushidhananjani@gmail.com>
2025-02-20 16:56:38 +0530

    fmt

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "comin-main_server" {
   key_name               = aws_key_pair.aws_key.id
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.hr_system_sg.id]
-  user_data = file("userdata.tpl")
+  user_data              = file("userdata.tpl")
 
   root_block_device {
     volume_size = 8
@@ -15,5 +15,5 @@ resource "aws_instance" "comin-main_server" {
     Name = "comin_main-server"
   }
 
-  
+
 }
